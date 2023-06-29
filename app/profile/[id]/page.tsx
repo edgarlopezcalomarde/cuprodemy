76e59e91a310337cf0c91236136dda72b53c651b
Edgar Lopez Calomarde <edgarlopezcalomarde@gmail.com>
2023-06-29 11:41:34 +0200

    public profile changes

--- app/profile/[id]/page.tsx
+++ app/profile/[id]/page.tsx
@@ -1,5 +1,8 @@
-import Profile from "@components/Profile";
+"use client"
 
+import Profile from "@components/Profile";
+import { Course } from "@interfaces/course";
+import { useEffect, useState } from "react";
 
 interface PublicProfilePageProps {
   params: { id: string };
@@ -8,27 +11,28 @@ interface PublicProfilePageProps {
 export default async function PublicProfilePage({
   params,
 }: PublicProfilePageProps) {
+
   const { id } = params;
 
-  const fetchCourses = async () => {
-    const response = await fetch(`http://127.0.0.1:3000/api/user/${id}/course`);
-    return await response.json();
-  };
+  const [courses, setCourses] = useState<Array<Course>>([]);
+  const [username, setUsername] = useState<string>("");
 
-  const fetchUser = async () => {
-    const response = await fetch(`http://127.0.0.1:3000/api/user/${id}`);
-    return await response.json();
-  };
+  useEffect(() => {
+    const fetchCoursesAndUsername = async () => {
+      const dataCourse = await fetch(`/api/user/${id}/course`);
+      const dataUser = await fetch(`/api/user/${id}`);
+      setCourses(await dataCourse.json());
+      setUsername(await dataUser.json());
+    };
 
-  
+    fetchCoursesAndUsername();
+  }, []);
 
-  const courses = await fetchCourses();
-  const user = await fetchUser();
 
   return (
     <>
       <h1 className="head_text text-left ">
-        <span className="blue_gradient">Profile of {user.username} </span>
+        <span className="blue_gradient">Profile of {username} </span>
       </h1>
 
       <Profile courses={courses}/>
