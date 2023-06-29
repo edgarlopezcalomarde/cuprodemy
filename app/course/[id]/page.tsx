ab391181e467df1278e42538e0423dac106ff25f
Edgar Lopez Calomarde <edgarlopezcalomarde@gmail.com>
2023-06-29 11:57:27 +0200

    fetch changed

--- app/course/[id]/page.tsx
+++ app/course/[id]/page.tsx
@@ -1,27 +1,33 @@
+"use client";
+
 import BackButton from "@components/BackButton";
 import { Course } from "@interfaces/course";
+import { useState, useEffect } from "react";
 
 interface CoursePageProps {
   params: { id: string };
 }
 
-export default async function CoursePage({ params }: CoursePageProps) {
-  const fetchCourse = async () => {
-    const data = await fetch("http://localhost:3000/api/course/" + params.id);
-    return await data.json();
-  };
+export default function CoursePage({ params }: CoursePageProps) {
+  const [course, setCourse] = useState<Course>();
 
-  const course: Course = await fetchCourse();
+  useEffect(() => {
+    const fetchCourse = async () => {
+      const data = await fetch("/api/course/" + params.id);
+      setCourse(await data.json());
+    };
+
+    fetchCourse();
+  }, []);
 
   return (
     <div className="flex flex-col gap-4 text-justify font-inter pb-10">
-
       <BackButton />
-      
-      <h2 className="font-bold text-3xl">{course.title}</h2>
-      <h3 className="font-medium text-xl text-slate-600">{course.author}</h3>
-      <p className="font-normal">{course.description}</p>
-      <pre className="w-">{course.content}</pre>
+
+      <h2 className="font-bold text-3xl">{course?.title}</h2>
+      <h3 className="font-medium text-xl text-slate-600">{course?.author}</h3>
+      <p className="font-normal">{course?.description}</p>
+      <pre className="w-">{course?.content}</pre>
     </div>
   );
 }
